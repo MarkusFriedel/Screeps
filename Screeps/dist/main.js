ea41475cd71d764c2945652035dcba0f763776a9
Markus Friedel <markus@wistema.de>
2016-08-11 07:07:15 +0200

    scout pathing changed

--- Screeps/dist/main.js
+++ Screeps/dist/main.js
@@ -7468,20 +7468,9 @@ var Scout = (function (_super) {
         try {
             //this.creep.say('SCOUT');
             this.memory = this.creep.memory;
-            if (!this.memory.path) {
-                var path = PathFinder.search(this.creep.pos, { pos: RoomPos.fromObj(this.memory.targetPosition), range: 10 }, { roomCallback: Colony.getTravelMatrix, plainCost: 1, swampCost: 1 });
-                path.path.unshift(this.creep.pos);
-                this.memory.path = path;
+            if (this.memory.targetPosition) {
+                this.moveTo({ pos: RoomPos.fromObj(this.memory.targetPosition), range: 20 });
             }
-            if (this.moveByPath() == ERR_INVALID_ARGS)
-                this.memory.path = null;
-            //let pos = this.creep.pos;
-            //if (this.memory.targetPosition!=null && (pos.roomName != this.memory.targetPosition.roomName || pos.x < 3 || pos.x > 46 || pos.y < 3 || pos.y > 46)) {
-            //    //let path = this.creep.pos.findPathTo(new RoomPosition(25, 25, this.memory.targetRoomName), { ignoreDestructibleStructures: true });
-            //    let result = this.creep.moveTo(new RoomPosition(25, 25, this.memory.targetPosition.roomName), { reusePath: 50 });
-            //    if (result == ERR_NO_PATH)
-            //        this.creep.suicide();
-            //}
             if (this.memory.targetPosition && this.creep.pos.roomName == this.memory.targetPosition.roomName) {
                 var myRoom = Colony.getRoom(this.creep.pos.roomName);
                 if (myRoom.memory.lst < Game.time - 100)
